a9f377feca06a67ce45b744ae9475a9fbb7d4424
Jihad Ghannoum <69480541+Ghannoum99@users.noreply.github.com>
2021-05-27 15:06:32 +0200

    Update Archer.java

--- src/modele/Archer.java
+++ src/modele/Archer.java
@@ -3,7 +3,7 @@ package modele;
 public class Archer extends Soldat{
 
 	public Archer(int abscisse, int ordonnees) {
-		super(abscisse, ordonnees, "images/personnages/archer.png", "archer", 500, 80, 70, 25, 100,false);
+		super(abscisse, ordonnees, "images/personnages/archer.png", "archer", "images/personnages/archerPivotee.png", 500, 80, 2, 25, 100,false);
 	}
 
 }
