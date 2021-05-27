44c585c5a05819a3c79dce56559b5df341d42b7c
Jihad Ghannoum <69480541+Ghannoum99@users.noreply.github.com>
2021-05-27 15:07:22 +0200

    Update InfanterieLourde.java

--- src/modele/InfanterieLourde.java
+++ src/modele/InfanterieLourde.java
@@ -2,6 +2,6 @@ package modele;
 
 public class InfanterieLourde extends Soldat{
 	public InfanterieLourde(int abscisse, int ordonnees) {
-		super(abscisse, ordonnees, "images/personnages/infanterie_lourde.png", "infanterieLourde", 200, 80, 70, 25, 100,false);
+		super(abscisse, ordonnees, "images/personnages/infanterie_lourde.png", "infanterieLourde", "images/personnages/infanterie_lourdePivotee.png", 200, 80, 70, 25, 100,false);
 	}
 }
