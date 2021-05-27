75134082418df915d1ff03b38e8be8a066386e47
Jihad Ghannoum <69480541+Ghannoum99@users.noreply.github.com>
2021-05-27 15:06:49 +0200

    Update Cavalerie.java

--- src/modele/Cavalerie.java
+++ src/modele/Cavalerie.java
@@ -3,7 +3,7 @@ package modele;
 public class Cavalerie extends Soldat{
 
 	public Cavalerie(int abscisse, int ordonnees) {
-		super(abscisse, ordonnees, "images/personnages/cavalerie.png", "cavalerie", 400, 80, 70, 25, 100,false);
+		super(abscisse, ordonnees, "images/personnages/cavalerie.png", "cavalerie", "images/personnages/cavaleriePivotee.png", 400, 80, 70, 25, 100,false);
 	}
 
 }
