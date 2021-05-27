b1e7a044a9a19b64bf52a81b7094dd05dd05c679
Jihad Ghannoum <69480541+Ghannoum99@users.noreply.github.com>
2021-05-27 15:05:24 +0200

    Update PlateauVue.java

--- src/vue/PlateauVue.java
+++ src/vue/PlateauVue.java
@@ -10,7 +10,7 @@ import java.util.*;
 import modele.*;
 
 /*
- * La classe PlateauVue permet d’afficher les différents éléments du plateau 
+ * La classe PlateauVue permet dâ€™afficher les diffÃ©rents Ã©lÃ©ments du plateau 
  * : les terrains, les soldats des joueurs de la partie et le cadre du plateau.
  */
 
@@ -20,11 +20,14 @@ public class PlateauVue extends JFrame implements KeyListener {
 	private PanelTerrains panelTerrains;
 	private SoldatVue soldatVue;
 	private PanelInfosSoldat panelInfosSoldat;
+	
+	private MiniMap minimap;
+	
 	private Joueur tourJoueur;
 	private ArrayList<Joueur> joueurs;
 	
 	public PlateauVue() {  
-		// Définition des données de la fenêtre principale
+		// DÃ©finition des donnÃ©es de la fenÃªtre principale
 		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		this.setBounds(0,0,1038, 790);
 		this.getContentPane().setBackground(Color.white);	
@@ -49,30 +52,36 @@ public class PlateauVue extends JFrame implements KeyListener {
 		//PanelFinBataille fin = new PanelFinBataille();
 		//this.add(fin);
 		
-		// Création du panel permettant d'afficher les infos du soldat
+		// CrÃ©ation du panel permettant d'afficher les infos du soldat
 		this.panelInfosSoldat = new PanelInfosSoldat();
 		getContentPane().add(this.panelInfosSoldat);
 		
-		// Création des joueurs (juste pour faire les tests)
+		// CrÃ©ation de minimap
+		 this.minimap = new MiniMap(this.joueurs, this.tourJoueur);
+	     //this.minimap.AfficherMiniSoldat();
+	     getContentPane().add(this.minimap);
+
+		
+		// CrÃ©ation des joueurs (juste pour faire les tests)
 		this.joueurs = new ArrayList<Joueur>();
 		this.joueurs.add(new Joueur("bobby", new ArrayList<Soldat>(), 0, ""));
 		this.joueurs.add(new Joueur("bobby", new ArrayList<Soldat>(), 0, ""));
 		this.joueurs.add(new Joueur("bobby", new ArrayList<Soldat>(), 0, ""));
 		this.joueurs.add(new Joueur("bobby", new ArrayList<Soldat>(), 0, ""));
 		
-		// Création des labels représentant les soldats
+		// CrÃ©ation des labels reprÃ©sentant les soldats
 		this.soldatVue = new SoldatVue();
 		this.soldatVue.creerSoldats(this.joueurs);
 		
-		// Choix aléatoire d'un joueur pour commencer le tour
+		// Choix alÃ©atoire d'un joueur pour commencer le tour
 		int ind =(int) (Math.random() * (this.joueurs.size() - 0));
 		this.tourJoueur = this.joueurs.get(ind);
 	
-		// Création du panel permettant d'afficher les terrains et de positionner les soldats
+		// CrÃ©ation du panel permettant d'afficher les terrains et de positionner les soldats
 		this.panelTerrains = new PanelTerrains(this.tourJoueur, this.soldatVue, this.panelInfosSoldat);
 		getContentPane().add(this.panelTerrains.getScrollPane());
 		
-		
+	
 		
 		SwingUtilities.updateComponentTreeUI(this);
 	}
@@ -89,7 +98,6 @@ public class PlateauVue extends JFrame implements KeyListener {
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		this.panelTerrains.keyPressed(e);
 		// Pour l'instant il faut appuyer sur la touche espace pour changer de joueur (juste pour les tests)
 		if(e.getKeyCode()==KeyEvent.VK_SPACE){
 			System.out.println("Changement de joueur");
