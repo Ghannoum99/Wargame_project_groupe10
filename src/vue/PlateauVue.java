70e43b1459f812f18e6a25b1dfcc143ae6a50abf
Jihad Ghannoum <69480541+Ghannoum99@users.noreply.github.com>
2021-05-31 17:43:31 +0200

    Update PlateauVue.java

--- src/vue/PlateauVue.java
+++ src/vue/PlateauVue.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 
 import java.util.*;
 import modele.*;
@@ -31,6 +33,15 @@ public class PlateauVue extends JFrame {
 	private ArrayList<Joueur> joueurs;
 	private Guide guide;
 	private boolean clicked = false;
+	private JButton boutonQuitter;
+	private JButton boutonReJouer;
+	public JLabel labelInfo;
+	private JPanel panelInfo;		
+	private JLabel labelTitre;
+	private JLabel labelScore;
+	private JLabel labelNumJoueur;
+	private JLabel labelNomJoueur;
+	
 
 	public PlateauVue(ArrayList<Joueur> joueurs, String scenario) { 
 		// Définition des données de la fenêtre principale
@@ -270,11 +281,145 @@ public class PlateauVue extends JFrame {
 		}
 
 		if (termine) {
-			PanelFinBataille fin = new PanelFinBataille(joueurs, gagnant);
-			this.plateau.add(fin, JLayeredPane.DRAG_LAYER);
+			afficherPanelFinBataille();
 			panelTerrains.retirerMouseListenerHexagones(); 
 		}
 
+	}
+	
+	/********************************************************************/
+	/** AFFICHAGE D'UN BOUTON QUI PERMET AUX JOUEURS DE QUITTER LE JEU **/
+	/********************************************************************/		
+	public void afficherBoutonQuitter() {
+		boutonQuitter = new JButton(new ImageIcon("images/large-button-active.png"));
+		boutonQuitter.setText("Quitter");
+		boutonQuitter.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		boutonQuitter.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		boutonQuitter.setForeground(Color.white);
+		boutonQuitter.setHorizontalTextPosition(JButton.CENTER);
+		boutonQuitter.setBounds(289, 370, 172, 48);
+		this.add(boutonQuitter);
+	}		
+	
+	/*************************************************************************************************/
+	/** AFFICHAGE D'UN BOUTON, QUI RE-AFFICHE LE MenuPrincipal POUR RE-COMMENCER UNE AUTRE BATAILLE **/
+	/*************************************************************************************************/
+	public void afficherBoutonRejouer() {
+		boutonReJouer = new JButton();
+		boutonReJouer.setText("Re-Jouer");
+		boutonReJouer.setIcon(new ImageIcon("images/large-button-active.png"));
+		boutonReJouer.addActionListener(new ActionListener() {
+			@SuppressWarnings("deprecation")
+			public void actionPerformed(ActionEvent e) {
+				MenuPrincipal menuPrincipal = new MenuPrincipal();
+				menuPrincipal.show();
+			}
+		});
+		boutonReJouer.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		boutonReJouer.setForeground(Color.white);
+		boutonReJouer.setHorizontalTextPosition(JButton.CENTER);
+		boutonReJouer.setBounds(79, 370, 172, 48);
+		this.add(boutonReJouer);
+	}	
+	
+	/****************************************************************************************/
+	/** AFFICHAGE D'UN PANEL, QUI CONTIENT LES INFORMATIONS (NOM DES JOUEURS, LEUR SCORES) **/
+	/****************************************************************************************/		
+	public void afficherPanelInfo() {
+		panelInfo = new JPanel();
+		panelInfo.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Scores", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panelInfo.setBounds(59, 84, 424, 263);
+		this.add(panelInfo);
+		panelInfo.setLayout(null);
+	}
+	
+	/******************************************************************/
+	/** AFFICHAGE DE 3 LABELS, CHAQUE LABEL CORRESPOND A UNE COLONEE **/
+	/******************************************************************/
+	public void afficherLabelInfo() {
+		int i = 0;
+		int x = 50;
+		String[] text = {"#Num", "Joueur", "Score"};
+		for(i=0; i<3; i++) {
+			labelInfo = new JLabel(text[i]);
+			labelInfo.setFont(new Font("Times new Roman", Font.BOLD, 17));
+			labelInfo.setForeground(Color.GRAY);
+			labelInfo.setBounds(x, 105, 70, 41);
+			this.add(labelInfo);
+			
+			x += 190;
+		}
+	}
+	
+	public void afficherScore() {
+		int y = 154;
+		for(int i = 0; i < joueurs.size(); i++) {
+			String score = String.valueOf((Integer) joueurs.get(i).getScore());
+			labelScore = new JLabel();
+			labelScore.setText(score);
+			labelScore.setForeground(new Color(200, 173, 10));
+			labelScore.setFont(new Font("Times new Roman", Font.PLAIN, 15));
+			labelScore.setBounds(448, y, 77, 41);
+			this.add(labelScore);
+			
+			y += 33;
+		}
+	}
+	
+	public void afficherNumJoueur() {
+		int y = 154;
+		for(int i = 0; i < joueurs.size(); i++) {
+			String numJoueur = String.valueOf((Integer) i);
+			labelNumJoueur = new JLabel();
+			labelNumJoueur.setText(numJoueur);
+			labelNumJoueur.setForeground(new Color(200, 173, 10));
+			labelNumJoueur.setFont(new Font("Times new Roman", Font.PLAIN, 15));
+			labelNumJoueur.setBounds(68, y, 77, 41);
+			this.add(labelNumJoueur);
+			
+			y += 33;
+		}
+	}
+	
+	public void afficherNomJoueur() {
+		int y = 154;
+		for(int i = 0; i < joueurs.size(); i++) {
+			labelNomJoueur = new JLabel();
+			labelNomJoueur.setText(joueurs.get(i).getNomJoueur());
+			labelNomJoueur.setForeground(new Color(200, 173, 10));
+			labelNomJoueur.setFont(new Font("Times new Roman", Font.PLAIN, 16));
+			labelNomJoueur.setBounds(239, y, 77, 41);
+			this.add(labelNomJoueur);
+			
+			y += 33;
+		}
+	}
+	
+	
+	public void afficherPanelFinBataille() {
+		PanelMenuInfos panelMenu = new PanelMenuInfos(290, 127, 596, 480);
+		this.plateau.add(panelMenu, JLayeredPane.DRAG_LAYER);
+		
+		/** TITRE DU PANEL **/
+		String felicitation = "Félicitations" + " " + gagnant.getNomJoueur();
+		labelTitre = new JLabel(felicitation);
+		labelTitre.setForeground(new Color(200, 173, 10));
+		labelTitre.setFont(new Font("Times new Roman", Font.BOLD, 20));
+		labelTitre.setBounds(168, 10, 209, 41);
+		this.add(labelTitre);
+		
+		/** AFFICHAGE DU PANEL QUI CONTIENT LES INFORMATIONS (Numéro du joueur, Nom du Joueur et son score) **/
+		//afficherPanelInfo();
+		afficherLabelInfo();
+		afficherNumJoueur();
+		afficherNomJoueur();
+		afficherScore();
+		afficherBoutonRejouer();
+		afficherBoutonQuitter();
 	}
 
 	public Joueur getTourJoueur() {
