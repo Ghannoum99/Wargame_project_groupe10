439c4ecdf7ff61040bc0cf7dc22901aac20fbe70
Jihad Ghannoum <69480541+Ghannoum99@users.noreply.github.com>
2021-06-02 00:25:43 +0200

    Update PanelInfosSoldat.java

--- src/vue/PanelInfosSoldat.java
+++ src/vue/PanelInfosSoldat.java
@@ -26,31 +26,36 @@ public class PanelInfosSoldat extends JPanel {
 		attaqueLabel.setBounds(0,100, 80, 20);
 		this.attaque = new JLabel();
 		this.attaque.setBounds(60,101, 40, 20);
+		this.attaque.setForeground(new Color(200, 173, 10));
 
 		JLabel defenseLabel = new JLabel("Defense : ");
 		defenseLabel.setForeground(Color.white);
 		defenseLabel.setBounds(0,130, 80, 20);
 		this.defense = new JLabel();
 		this.defense.setBounds(65,131, 80, 20);
-
+		this.defense.setForeground(new Color(200, 173, 10));
+		
 		JLabel deplacementLabel = new JLabel("Deplacement : ");
 		deplacementLabel.setForeground(Color.white);
 		deplacementLabel.setBounds(0,160, 90, 20);
 		this.deplacement = new JLabel();
 		this.deplacement.setBounds(90,161, 90, 20);
+		this.deplacement.setForeground(new Color(200, 173, 10));
 
 		JLabel visionLabel = new JLabel("Vision : ");
 		visionLabel.setForeground(Color.white);
 		visionLabel.setBounds(0,190, 80, 20);
 		this.vision = new JLabel();
 		this.vision.setBounds(50,191, 80, 20);
+		this.vision.setForeground(new Color(200, 173, 10));
 
 		JLabel pvLabel = new JLabel("Point de vie : ");
 		pvLabel.setForeground(Color.white);
 		pvLabel.setBounds(0,220, 90, 20);
 		this.pv = new JLabel();
 		this.pv.setBounds(85,221, 90, 20);
-
+		this.pv.setForeground(new Color(200, 173, 10));
+		
 		this.add(labelImage);
 		this.add(attaqueLabel);
 		this.add(attaque);
